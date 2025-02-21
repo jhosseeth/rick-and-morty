201f5b77542b37eb3aea005a395ec1379bdd23e8
Jhoss Naranjo <djosephn@gmail.com>
2025-02-20 23:07:10 -0500

    Fix basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <FavoritesProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/rick-and-morty">
           <App />
         </BrowserRouter>
       </FavoritesProvider>
