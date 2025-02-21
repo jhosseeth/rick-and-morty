38f0be210ea8dabc55fffc3eb81e22cea4e32984
Jhoss Naranjo <djosephn@gmail.com>
2025-02-20 19:51:52 -0500

    Add filter and search icons

--- src/components/characters/search/Search.tsx
+++ src/components/characters/search/Search.tsx
@@ -1,3 +1,6 @@
+import filterIcon from '../../../assets/icons/filter.svg';
+import searchIcon from '../../../assets/icons/search.svg';
+
 interface SearchProps {
   value: string;
   onChange: (value: string) => void;
@@ -9,14 +12,23 @@ function Search({ value, onChange }: SearchProps) {
       <input
         type="text"
         placeholder="Search or filter results"
-        className="w-full p-3 pl-10 rounded-lg bg-gray-100 border border-gray-200"
+        className="w-full p-3 px-10 rounded-lg bg-gray-100 border border-gray-200"
         value={value}
         onChange={(e) => onChange(e.target.value)}
       />
       <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
-        <svg className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-        </svg>
+        <img 
+          src={searchIcon} 
+          alt="Toogle favorite" 
+          className={`w-5 h-5`}
+        />
+      </div>
+      <div className="absolute inset-y-0 right-3 flex items-center" onClick={() => console.log('Click')}>
+        <img 
+          src={filterIcon} 
+          alt="Toogle favorite" 
+          className={`w-5 h-5 cursor-pointer`}
+        />
       </div>
     </div>
   );
